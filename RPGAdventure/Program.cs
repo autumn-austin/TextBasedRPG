b94a47744a9ab56788f4c9a9bc1b81784f75ae6c
Autumn Austin <a.austin0119@gmail.com>
2022-03-04 08:32:04 -0500

    Intialized setup files

---
+++ RPGAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RPGAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
