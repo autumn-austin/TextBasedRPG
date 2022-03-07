5550f47712bf8fe7df7908f9d35ff43539113799
Autumn Austin <a.austin0119@gmail.com>
2022-03-07 17:23:03 -0500

    Added multiple layers to enemy classes, continued story

---
+++ RPGAdventure/Ghostopher.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGAdventure
+{
+    class Ghostopher
+    {
+        static Random rand = new Random();
+        public static void EnemyGhostopher()
+        {
+            GhostopherBattle("Ghostopher", 2, 5);
+
+        }
+
+        public static void GhostopherBattle(string name, int power, int health)
+        {
+            string n = "Ghostopher";
+            int p = 2;
+            int h = 5;
+
+            n = name;
+            p = power;
+            h = health;
+
+            while (h > 0)
+            {
+                Console.WriteLine(n);
+                Console.WriteLine("Power: " + p);
+                Console.WriteLine("Health: " + h);
+                Console.WriteLine("     .-.");
+                Console.WriteLine("   .'   `.");
+                Console.WriteLine("   :g g   :");
+                Console.WriteLine("   : o    `.");
+                Console.WriteLine("  :         ``.");
+                Console.WriteLine(" :             `.");
+                Console.WriteLine(":  :         .   `.");
+                Console.WriteLine(":   :          ` . `.");
+                Console.WriteLine(" `.. :            `. ``;");
+                Console.WriteLine("    `:;             `:'");
+                Console.WriteLine("       :              `.");
+                Console.WriteLine("        `.              `.     .");
+                Console.WriteLine("         `'`'`'`---..,___`;.-'");
+                Console.WriteLine("        'Oooooo Spooooooky'      ");
+                Console.WriteLine("===============================");
+                Console.WriteLine("");
+                Console.WriteLine("========================");
+                Console.WriteLine("~ (A)ttack    (S)peak  ~");
+                Console.WriteLine("~ (D)efend    (F)lee   ~");
+                Console.WriteLine("~        (H)eal        ~");
+                Console.WriteLine("========================");
+                Console.WriteLine("Potions: " + Program.currentPlayer.potion + " Health: " + Program.currentPlayer.health);
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "a" || input.ToLower() == "attack")
+                {
+                    //attack
+                    Console.WriteLine("You smack " + n + " with your torch." + n + " doesn't feel anything.");
+                    Console.WriteLine("You forgot " + n + " is a ghost...");
+
+                    int damage = p - Program.currentPlayer.armorValue;
+                    if (damage < 0)
+                        damage = 0;
+
+                    int attack = rand.Next(0, Program.currentPlayer.weaponValue) + rand.Next(1, 4);
+
+                    Console.WriteLine("You lose " + damage + " health and deal " + attack + " damage.");
+
+                    Program.currentPlayer.health -= damage;
+                    h -= attack;
+
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input.ToLower() == "s" || input.ToLower() == "speak")
+                {
+                    //speak
+                    Console.WriteLine("You attempt to gain information from " + n + " but " + n + " doesn't want to chat.");
+                    Console.ReadKey();
+                    Console.WriteLine("'Tra La La'");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input.ToLower() == "d" || input.ToLower() == "defend")
+                {
+                    int damage = (p / 4) - Program.currentPlayer.armorValue;
+                    if (damage < 0)
+                        damage = 0;
+
+                    int attack = rand.Next(0, Program.currentPlayer.weaponValue) / 2;
+
+                    Console.WriteLine("You lose sight of " + n + " but " + n + " feel a cold spot on your head.");
+                    Console.ReadKey();
+                    Console.WriteLine("'I'm your hat now.'");
+                    Console.ReadKey();
+                    Console.WriteLine("You lose " + damage + " health from shivering and deal " + attack + " damage to " + n + "'s sheet. It's egyptian cotton.");
+
+                    Program.currentPlayer.health -= damage;
+                    h -= attack;
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input.ToLower() == "h" || input.ToLower() == "heal")
+                {
+                    //heal
+                    if (Program.currentPlayer.potion == 0)
+                    {
+                        Console.WriteLine("You frantically search your pockets...");
+                        Console.WriteLine("...pocket lint...");
+                        int damage = p - Program.currentPlayer.armorValue;
+                        if (damage < 0)
+                            damage = 0;
+
+                        Console.WriteLine("You take " + damage + "and " + n + "wriggles around excitedly.");
+
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("You frantically search your pockets...");
+                        Console.WriteLine("A giant red vial you pull from your pocket.");
+                        Console.WriteLine("How did that fit in there?\nYou drink the vial.");
+                        int potionValue = 5;
+                        Console.WriteLine("You gain " + potionValue + " health and get the jitters.");
+                        Program.currentPlayer.health += potionValue;
+                        Program.currentPlayer.potion -= 1;
+                        Console.WriteLine("You had your back turned searching your pockets...");
+                        Console.WriteLine(n + " strikes you in the rear. You blush...");
+                        int damage = (p / 2) - Program.currentPlayer.armorValue;
+                        if (damage < 0)
+                            damage = 0;
+                        Console.WriteLine("You lose " + damage + " health.");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input.ToLower() == "f" || input.ToLower() == "flee")
+                {
+                    //flee
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        Console.WriteLine("You attempt to flee... but it looks like " + n + " has a strong gaze upon you.");
+                        Console.WriteLine("I don't think you are going anywhere...");
+                        int damage = p - Program.currentPlayer.armorValue;
+
+                        if (damage < 0)
+                            damage = 0;
+
+                        Console.WriteLine("You lose " + damage + " health, and are unable to escape.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("You use your charm and wit to distract the " + n + " as you slink away to safety.");
+                        Console.ReadKey();
+                        Town.LoadTown(Program.currentPlayer);
+                        // CALL GO TO TOWN, FROM TOWN CAN ENTER STORE, CREATE CLASS FOR TOWN
+                    }
+                }
+                if (Program.currentPlayer.health <= 0)
+                {
+                    //Death Code
+                    Console.WriteLine("The " + n + " has killed you... Don't give up now" + Program.currentPlayer.name + "!");
+                    Console.ReadKey();
+                    System.Environment.Exit(0);
+                }
+                Console.ReadKey();
+            }
+
+            int c = Program.currentPlayer.GetCoins();
+            Console.WriteLine("You killed " + n + " and received " + c + " gold.");
+            Console.ReadKey();
+            Console.WriteLine("Don't spend it all in one place.");
+            Program.currentPlayer.gold += c;
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
--- RPGAdventure/Story.cs
+++ RPGAdventure/Story.cs
@@ -10,52 +10,99 @@ namespace RPGAdventure
     {
         public static void Start()
         {
-            Console.WriteLine("====================");
-            Console.WriteLine("=                  =");
-            Console.WriteLine("=     WELCOME      =");
-            Console.WriteLine("=                  =");
-            Console.WriteLine("====================");
+            Console.WriteLine("===============================WELCOME=====================================");
+            Console.WriteLine("  *                  *          +    *      *       *       *          *");
+            Console.WriteLine("  *             *               |      *       *        *       *    *  ");
+            Console.WriteLine("     *   *              *     _|_|_              *         *      *     ");
+            Console.WriteLine(" _____    *    ____  *  _____|     |______________   *  _________   *   * ");
+            Console.WriteLine("|o o o|_______|    |___|               | | # # #  |____| o o o o |   (|)");
+            Console.WriteLine("|o o o|  * * *|: ::|. .|               |o| # # #  |. . | o o o o |  ((|))");
+            Console.WriteLine("|o o o|* * *  |::  |. .| []  []  []  []|o| # # #  |. . | o o o o | (((|)))");
+            Console.WriteLine("|o o o|**  ** |:  :|. .| []  []  []    |o| # # #  |. . | o o o o |((((|))))");
+            Console.WriteLine("|_[]__|__[]___|_||_|__<|____________;;_|_|___[]___|_.|_|____[]___|    |");
             Console.ReadKey();
             Console.Clear();
+
             Console.WriteLine("Enter Player Name: ");
             Program.currentPlayer.name = Console.ReadLine();
             Console.Clear();
             Console.WriteLine("You awake in a rainy, dark forest. You feel dazed and are having trouble remembering");
             Console.WriteLine("anything about how you got here...");
+
             if (Program.currentPlayer.name == "")
+            {
                 Console.WriteLine("Oh...you can't even remember your own name...");
+                Program.currentPlayer.name = "Harrietta Styluspen";
+                Console.ReadKey();
+
+                Console.Clear();
+                Console.WriteLine("You feel around in the darkness until you find a previously lit torch...");
+                Console.WriteLine("You aren't sure how you are going to light this in a rainy forest to illuminate your way.");
+                Console.ReadKey();
+                Console.WriteLine("You then hear rustling coming from behind you.");
+                Console.ReadKey();
+                Console.WriteLine("Shakenly, you turn...");
+                Console.ReadKey();
+                Console.Clear();
+            }
             else
+            {
                 Console.WriteLine($"You know your name is {Program.currentPlayer.name}.");
-            Console.ReadKey();
-            Console.Clear();
-            Console.WriteLine("You feel around in the darkness until you find a previously lit torch...");
-            Console.WriteLine("You aren't sure how you are going to light this in a rainy forest to illuminate your way.");
-            Console.ReadKey();
-            Console.WriteLine("You then hear rustling coming from behind you.");
-            Console.ReadKey();
-            Console.WriteLine("Shakenly, you turn...");
-            Console.ReadKey();
+                Console.ReadKey();
+
+                Console.Clear();
+                Console.WriteLine("You feel around in the darkness until you find a previously lit torch...");
+                Console.WriteLine("You aren't sure how you are going to light this in a rainy forest to illuminate your way.");
+                Console.ReadKey();
+                Console.WriteLine("You then hear rustling coming from behind you.");
+                Console.ReadKey();
+                Console.WriteLine("Shakenly, you turn...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+               
         }
 
         public static void MayorEncounter()
         {
             Console.WriteLine("You hear a shout from a distance and see a shadow running toward you.");
             Console.WriteLine("You hold up your torch in defense when a soft voice approaches");
-            Console.WriteLine("'Hello, there! I heard some ruckus in the distance and thought someone surely was in trouble.");
-            Console.WriteLine("It seems to be a Mayor?... A very short and round fellow.");
-            Console.WriteLine("'You must be " + Program.currentPlayer.name + "!'");
-            Console.WriteLine("How does this rotund man know who you are...?");
-            Console.WriteLine("'We've been waiting for you...");
             Console.ReadKey();
+            Console.WriteLine("'Hello, there! I heard some ruckus in the distance and thought someone surely was in trouble.");
+            Console.ReadKey();
+            Console.WriteLine("It seems to be a Mayor?... A very short and round fellow.");
+            Console.WriteLine($"'You must be {Program.currentPlayer.name}!'");
+            Console.ReadKey();
+            Console.WriteLine("How does this rotund man know who you are...?");
+            Console.ReadKey();
+            Console.WriteLine("'We've been waiting for you...'");
+            Console.ReadKey();
+            Console.Clear();
         }
 
         public static void TownGreeting()
         {
             Console.Write("You follow this Mayor that somewhat resembles a deflated basketball.");
             Console.Write("'This here is the town square, you see!'\n'We've set you up with an apartment to rest.'");
+            Console.ReadKey();
             Console.Write("You wonder why the hospitality of this place is so great, you don't know these people.");
+            Console.ReadKey();
             Console.Write("A woman with a tangled mess of hair approaches you... You notice there is a birds nest in her bun.");
-            Console.Write("'You must be!" + Program.currentPlayer.name + "'");
+            Console.Write($"'You must be {Program.currentPlayer.name}!'");
+            Console.ReadKey();
+            Console.WriteLine("This all seems very strange to you... Why does this strange woman have a birds nest in her bun?");
+            Console.ReadKey();
+            Console.WriteLine("'I bet you're wondering why we brought you here, huh?'");
+            Console.ReadKey();
+            Console.WriteLine("Yes.. of course you question the motive of this strange place and these strange but.. unatturally hospitable people.");
+            JudeGreeting();
+
+        }
+
+        public static void JudeGreeting()
+        {
+            Console.Clear();
+            Console.WriteLine($"'Well, well, well! Look what the cat bagged up and drug in! It's {Program.currentPlayer.name}!'");
         }
     }
 }
